5fa73c25eebcdf4c44c3166bbff889652ad358af
Adam Paszternak <hello@paszternak.me>
2014-01-21 08:07:25 +0100

    Update posts.php
    latin-ext chars in date string formats badly - added utf8_encode

--- citizen-dropplets/posts.php
+++ citizen-dropplets/posts.php
@@ -4,7 +4,7 @@
 		<h1 id="post-title"><a href="<?php echo($post_link); ?>" title="<?php echo($post_title); ?>"><?php echo($post_title); ?></a></h1>
 		<div class="post-meta">
 			<p>Written by <a href="http://twitter.com/<?php echo($post_author_twitter); ?>"><?php echo($post_author_twitter); ?></a></p>
-			<p>Published: <span><?php echo($published_date); ?></span></p>
+			<p>Published: <span><?php echo utf8_encode($published_date); ?></span></p>
 			<p>About <span><a href="<?php echo($post_category_link); ?>"><?php echo($post_category); ?></a></span></p>
 		</div>
 	</header>
